bac928337e8a1d2dcadeb969a5f82c9f33e527ac
SalmaBorrego <BorregoSYR06@outlook.es>
2019-01-23 09:26:03 -0600

    salmagilberto

---
+++ Tarea_1/Tarea_1/miconexion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea_1.Properties;
+
+namespace Tarea_1
+{
+    class miconexion
+    {
+        public static string ObtenerStringdeConexion()
+        {
+            return Settings.Default.setting;
+        }
+        public static string conexion = ObtenerStringdeConexion();
+    }
+}
